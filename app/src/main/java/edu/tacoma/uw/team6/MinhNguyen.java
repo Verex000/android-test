f7b9bd9f6b4b004a46908661d714140d8d290b36
Minh Nguyen <tmnx@users.noreply.github.com>
2020-04-11 22:09:43 -0700

    Minh's Activity commit fixed

--- app/src/main/java/edu/tacoma/uw/team6/MinhNguyen.java
+++ app/src/main/java/edu/tacoma/uw/team6/MinhNguyen.java
@@ -3,6 +3,7 @@ package edu.tacoma.uw.team6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MinhNguyen extends AppCompatActivity {
 
@@ -10,5 +11,8 @@ public class MinhNguyen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_minh_nguyen);
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText("This is Minh's Activity");
     }
 }
