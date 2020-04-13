a5b12e0cbf79c79c6cf74db5ee0740d9e4dcdf5d
ssmith149 <sesmith149@gmail.com>
2020-04-13 16:47:19 -0700

    sean smith commit

--- app/src/main/java/edu/tacoma/uw/team6/SeanSmith.java
+++ app/src/main/java/edu/tacoma/uw/team6/SeanSmith.java
@@ -10,5 +10,6 @@ public class SeanSmith extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sean_smith);
+        //comment
     }
 }
